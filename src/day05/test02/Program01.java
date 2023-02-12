562af70fcfc93d535b20139a7740b7bef45a99b3
J.Aravindh raj <aravindhrajber@gmail.com>
2023-02-12 20:39:06 +0530

    day05.test02

---
+++ src/day05/test02/Program01.java
@@ -0,0 +1,15 @@
+package day05.test02;
+
+public class Program01 {
+	
+	public static void display1()
+	{
+		System.out.println("Good Day...!");
+	}
+	
+	public void display1Nonstatic()
+	{
+		System.out.println("Good Day...!");
+	}
+
+}
---
+++ src/day05/test02/Program03.java
@@ -0,0 +1,13 @@
+package day05.test02;
+
+public class Program03 extends Program02{
+	
+	public static void main(String[] args) {
+		display1();
+		display2();
+		Program03 x=new Program03();
+		x.display1Nonstatic();//program01-class 
+		x.display2Nonstatic();//program02-class
+	}
+
+}
