fc6dc40f5579db1474acca0e0d3fa1b628390e0f
J.Aravindh raj <aravindhrajber@gmail.com>
2023-02-13 20:11:32 +0530

    day06.test01

---
+++ src/day06/test01/Program01.java
@@ -0,0 +1,32 @@
+package day06.test01;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		// + - * /(Q) %(R) Numbers
+		int a=20;
+		int b=3;
+		double c=3;
+		
+		// int () int--->int
+		// int () double/float--->double/float
+		
+		System.out.println(a+b);
+		System.out.println(a+c);
+		
+		System.out.println(a-b);
+		System.out.println(a-c);
+		
+		System.out.println(a*b);
+		System.out.println(a*c);
+		
+		System.out.println(a/b);
+		System.out.println(a/c);
+		
+		System.out.println(a%b);
+		System.out.println(a%c);
+		
+
+	}
+
+}
---
+++ src/day06/test01/Program02.java
@@ -0,0 +1,43 @@
+package day06.test01;
+
+import java.util.Scanner;
+
+public class Program02 {
+	
+	public static void main(String[] args) {
+		// + - * /(Q) %(R) Numbers
+		Scanner x=new Scanner(System.in);
+		
+		System.out.println("Entrer int value of a:-");
+		int a=x.nextInt();
+		
+		System.out.println("Entrer int value of a:-");
+		int b=x.nextInt();
+		
+		System.out.println("Entrer double value of a:-");
+		double c=x.nextDouble();
+		x.close();45
+		
+		// int () int--->int
+		// int () double/float--->double/float
+		
+		System.out.println(a+b);
+		System.out.println(a+c);
+		
+		System.out.println(a-b);
+		System.out.println(a-c);
+		
+		System.out.println(a*b);
+		System.out.println(a*c);
+		
+		System.out.println(a/b);
+		System.out.println(a/c);
+		
+		System.out.println(a%b);
+		System.out.println(a%c);
+		
+
+	}
+
+
+}
