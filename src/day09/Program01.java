57e88ba79de4c228413874c315d1734ec244bc13
J.Aravindh raj <aravindhrajber@gmail.com>
2023-02-22 09:02:21 +0530

    day09

---
+++ src/day09/Program01.java
@@ -0,0 +1,34 @@
+package day09;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start Program...!");
+		
+		int a=1;
+		while(a<=5)
+		{
+			System.out.println("Good Morning...!");
+			a=a+1;
+		}
+		
+		double b=0.5;
+		while(b>+0.1)
+		{
+			System.out.println("Good Day....!");
+			b=b-0.1;
+		}
+		
+		int c=-11;
+		while(c>=-15)
+		{
+			System.out.println("$$$$$$$$$$$$$$");
+			c=c-1;
+		}
+		
+		System.out.println("End Program....!");
+
+	}
+
+}
