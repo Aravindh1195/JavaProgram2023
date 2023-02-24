cf16d06adf7b58c959dcad2f82c5ee8711e07653
J.Aravindh raj <aravindhrajber@gmail.com>
2023-02-24 20:41:10 +0530

    day11

---
+++ src/day11/Program03.java
@@ -0,0 +1,42 @@
+package day11;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 1
+		 12
+		 123
+		 1234
+		 12345
+		 
+		 12345
+		 1234
+		 123
+		 12
+		 1
+		 */
+		
+		for(int i=1;i<=5;i++)//12345
+		{
+		for(int j=1;j<=i;j++)
+			{
+			System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		
+			for(int i=5;i>=1;i--)//54321
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ src/day11/Program04.java
@@ -0,0 +1,41 @@
+package day11;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 54321
+		 4321
+		 321
+		 21
+		 1
+		 
+		 1
+		 21
+		 321
+		 4321
+		 54321
+		 */
+		
+
+		for(int i=5;i>=1;i--)//54321
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+			
+		for(int i=1;i<=5;i++)//12345
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+			
+	}	
+}
